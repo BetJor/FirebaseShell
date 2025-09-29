f4d614ac7947b057fbe1e6f7cd0bcf01041ed0c6
Elisabet Jordana Quer <ejordana@costaisa.com>
2025-09-29 12:44:33 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,11 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  "projectId": "improvement-actions-manager",
-  "appId": "1:920139375274:web:3c4fd3616765e7480566d3",
-  "storageBucket": "improvement-actions-manager.firebasestorage.app",
-  "apiKey": "AIzaSyDb99QMKUYzyWyajTwZ2plmEB1o35g_9H4",
-  "authDomain": "improvement-actions-manager.firebaseapp.com",
-  "messagingSenderId": "920139375274"
+  "projectId": "actions-plus-64181820-69508",
+  "appId": "1:683988267197:web:cf51a20f0cbc8a070d47d4",
+  "apiKey": "AIzaSyCR3BqpFS2XqHEKD5C_tGEr6J7Nq6l6HvU",
+  "authDomain": "actions-plus-64181820-69508.firebaseapp.com",
+  "messagingSenderId": "683988267197"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
